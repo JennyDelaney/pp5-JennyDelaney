72f6c908b9490eaccd24c0c368d87391684fce41
JennyDelaney <>
2023-06-03 14:46:27 +0000

    Customized admin

--- tours/admin.py
+++ tours/admin.py
@@ -2,5 +2,26 @@ from django.contrib import admin
 from .models import Tour, Category
 
 # Register your models here.
-admin.site.register(Tour)
-admin.site.register(Category)
+
+class TourAdmin(admin.ModelAdmin):
+    list_display = (
+        'category',
+        'name',
+        'duration',
+        'price',
+        'rating',
+        'sku',
+        'image',
+    )
+
+    ordering = ('category',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Tour, TourAdmin)
+admin.site.register(Category, CategoryAdmin)
