16d67f0ed394a209c136818328e9468913cc7aa1
JennyDelaney <>
2023-06-04 13:26:06 +0000

    updated context processor

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,3 +1,5 @@
+from django.shortcuts import get_object_or_404
+from tours.models import Tour
 
 
 def bag_contents(request):
@@ -5,6 +7,17 @@ def bag_contents(request):
     bag_items = []
     total = 0
     tour_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        tour = get_object_or_404(Tour, pk=item_id)
+        total += quantity * tour.price
+        tour_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'tour': tour,
+        })
 
     grand_total = total
 
