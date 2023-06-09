303c04414ecc63c0df22325957c948e3d04264c2
JennyDelaney <>
2023-06-09 12:24:17 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51NAChwFlm6GdOBqtBRAI2MtnKTG4qy0kEzBrvwLRBilZ2WZHUMb4Rzj8NyaxgfwqBrVB67dNWeipaJChgw8MdXXz00RaJA6eQ2',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
