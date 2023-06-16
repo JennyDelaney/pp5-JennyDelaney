1a5ee3747d81fdd1df2afac6a61ebe0c6ad9c7cf
JennyDelaney <>
2023-06-16 18:25:48 +0000

    Fixed AWS Storage Bucket Name

--- all_tours/settings.py
+++ all_tours/settings.py
@@ -180,7 +180,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     # Bucket Config
-    AWS_STORAGE_BUKET_NAME = 'pp5-all-tours'
+    AWS_STORAGE_BUCKET_NAME = 'pp5-all-tours'
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
