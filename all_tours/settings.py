e050d9f9d3507f222c5dcff8198fcc1b553893c1
JennyDelaney <>
2023-06-16 23:58:12 +0000

    Change Debug to resolve Error 500 on Checkout

--- all_tours/settings.py
+++ all_tours/settings.py
@@ -26,7 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-jennydelaney-pp5-jennyde-9xgbi4rjjm.us2.codeanyapp.com',
                  'pp5-all-tours.herokuapp.com']
