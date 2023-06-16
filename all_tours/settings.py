95c9a4ff023e82169f4d7bf740ed759738520754
JennyDelaney <>
2023-06-16 21:12:42 +0000

    Shopping bag refactor

--- all_tours/settings.py
+++ all_tours/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+# settings.py
+from dotenv import load_dotenv 
+load_dotenv() 
+
 import os
 import dj_database_url
 
