72d990974ac1db3d5c31b4d5cb77170768dc7115
JennyDelaney <>
2023-06-16 21:25:31 +0000

    Removed .env file

--- all_tours/settings.py
+++ all_tours/settings.py
@@ -12,10 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
-# settings.py
-from dotenv import load_dotenv 
-load_dotenv() 
-
 import os
 import dj_database_url
 
