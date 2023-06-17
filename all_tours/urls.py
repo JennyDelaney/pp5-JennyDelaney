2080fcfd7381c674180b16eb3df11d8da3cb01cd
JennyDelaney <jennyelewis29@gmail.com>
2023-06-17 15:21:15 +0000

    Created Error 500.html

--- all_tours/urls.py
+++ all_tours/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import handler404
+from .views import handler404, handler500
 
 
 urlpatterns = [
@@ -33,3 +33,4 @@ urlpatterns = [
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'all_tours.views.handler404'
+handler500 = 'all_tours.views.handler500'
